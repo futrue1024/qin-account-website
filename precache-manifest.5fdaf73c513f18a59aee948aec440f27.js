1d22ec1b9f6e04a50d5e92365079eb64c0715109
chen1024 <indexchen@foxmail.com>
2020-09-11 12:26:28 +0800

    deploy

---
+++ precache-manifest.5fdaf73c513f18a59aee948aec440f27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f9a45c24747bd59d51",
+    "url": "/qin-account-website/css/app.720160c5.css"
+  },
+  {
+    "revision": "4e7093a7cfc9427de61049a6ae3c2a84",
+    "url": "/qin-account-website/img/记账.4e7093a7.png"
+  },
+  {
+    "revision": "10cd64f28a07ac661c9449b2e7df7274",
+    "url": "/qin-account-website/index.html"
+  },
+  {
+    "revision": "e3f9a45c24747bd59d51",
+    "url": "/qin-account-website/js/app.745ee077.js"
+  },
+  {
+    "revision": "79118a891ca4db6bfdf7",
+    "url": "/qin-account-website/js/chunk-vendors.778827ac.js"
+  },
+  {
+    "revision": "2389b9051ff3dfa899176100d61983f5",
+    "url": "/qin-account-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/qin-account-website/robots.txt"
+  }
+]);
